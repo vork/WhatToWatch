8384bc9382ea448d6d012c1a14886ca7fbc43617
Mark Boss <mark@zahlz.com>
2015-07-07 10:18:40 +0200

    WIP: request and display the trakt trending tv shows

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -18,6 +18,26 @@ angular
     'ngTouch',
     'ngMaterial'
   ])
+  .factory('TrendingShows', function($resource) {
+    return $resource(
+      'https://api-v2launch.trakt.tv/shows/trending',
+      {},
+      {
+        query: {
+          method:'GET',
+          isArray:true,
+          params: {
+            'extended':'images'
+          },
+          headers: {
+            'Content-type':'application/json',
+            'trakt-api-version':'2',
+            'trakt-api-key':'0c0a20790918c78c3a72c05da18de725509d9c4a428ad7dc0c4a523b7e33815c'
+          }
+        }
+      }
+    );
+  })
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,15 +8,73 @@
  * Controller of the projectApp
  */
 angular.module('projectApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, TrendingShows) {
+    console.log('controller call');
+    $scope.shows = [];
+    console.log($scope.shows);
+    TrendingShows.query().$promise.then(
+      function( trending ) {
+        $scope.isLoading = false;
+
+        var maxWatchers = 0;
+        //Find the maximum number of watchers
+        var i, len;
+        for(i = 0, len = trending.length; i < len; i++) {
+          if(maxWatchers < trending[i].watchers) {
+            maxWatchers = trending[i].watchers;
+          }
+        }
+        console.log('maxWatchers: ' + maxWatchers);
+        var toRet = [];
+        console.log('num of shows: ' + trending.length);
+        //Create a return type with the show poster and based on the watchers a col and row size
+        for(i = 0, len = trending.length; i < len; i++) {
+          var show = trending[i];
+          console.log(show);
+          toRet.push({
+            image: 'url(' + show.show.images.poster.medium + ')',
+            colspan: Math.ceil(show.watchers / maxWatchers * 2),
+            rowspan: Math.ceil(show.watchers / maxWatchers * 3)
+          });
+        }
+        console.log(toRet);
+        $scope.shows = toRet;
+      },
+      function( error ) {
+        alert("Something went wrong");
+      }
+    );
+    /*$scope.shows = (function() {
+      var trending = TrendingShows.query();
+      var maxWatchers = 0;
+      //Find the maximum number of watchers
+      var i, len;
+      for(i = 0, len = trending.length; i < len; i++) {
+        show = trending[i];
+        if(maxWatchers < show.watchers) {
+          maxWatchers = show.watchers;
+        }
+      }
+      console.log('maxWatchers: ' + maxWatchers);
+      var toRet = [];
+      console.log('num of shows: ' + trending.length);
+      //Create a return type with the show poster and based on the watchers a col and row size
+      for(i = 0, len = trending.length; i < len; i++) {
+        show = trending[i];
+        console.log(show);
+        toRet.push({
+          image: 'url(' + show.show.images.poster.medium + ')',
+          colspan: show.watchers / maxWatchers * 2,
+          rowspan : show.watchers / maxWatchers * 3
+        });
+      }
+      return toRet;
+    })();*/
   })
   .config( function($mdThemingProvider) {
     $mdThemingProvider.theme('docs-dark', 'default')
       .backgroundPalette('indigo')
-      .primaryPalette('pink');
+      .primaryPalette('pink')
+      .accentPalette('indigo');
   });
+
