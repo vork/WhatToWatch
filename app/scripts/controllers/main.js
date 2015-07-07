a2f4872ed266d3b165bea62a4aaf011dd89df244
Mark Boss <mark@zahlz.com>
2015-07-07 17:18:44 +0200

    Finished trending show grid

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,11 +9,11 @@
  */
 angular.module('projectApp')
   .controller('MainCtrl', function ($scope, TrendingShows) {
-    console.log('controller call');
-    $scope.shows = [];
-    console.log($scope.shows);
+    var _this = this;
+    _this.shows = [];
     TrendingShows.query().$promise.then(
       function( trending ) {
+        console.log("processing rest request");
         $scope.isLoading = false;
 
         var maxWatchers = 0;
@@ -24,26 +24,40 @@ angular.module('projectApp')
             maxWatchers = trending[i].watchers;
           }
         }
-        console.log('maxWatchers: ' + maxWatchers);
-        var toRet = [];
-        console.log('num of shows: ' + trending.length);
         //Create a return type with the show poster and based on the watchers a col and row size
         for(i = 0, len = trending.length; i < len; i++) {
           var show = trending[i];
-          console.log(show);
-          toRet.push({
-            image: 'url(' + show.show.images.poster.medium + ')',
-            colspan: Math.ceil(show.watchers / maxWatchers * 2),
-            rowspan: Math.ceil(show.watchers / maxWatchers * 3)
+          _this.shows.push({
+            image: show.show.images.poster.medium,
+            name: show.show.title,
+            colspan: Math.ceil(show.watchers / maxWatchers * 1.7),
+            rowspan: Math.ceil(show.watchers / maxWatchers * 1.5)
           });
         }
-        console.log(toRet);
-        $scope.shows = toRet;
+        console.log(_this.shows);
       },
       function( error ) {
         alert("Something went wrong");
       }
     );
+
+/*
+  this.shows = [];
+    console.log("Filling the shows array");
+    this.shows.push({
+     image: 'https://walter.trakt.us/images/shows/000/001/390/posters/medium/e2e8d04f11.jpg',
+     name: 'Game Of Thrones',
+     colspan: 2,
+     rowspan: 2
+   });
+   this.shows.push({
+      image: 'https://walter.trakt.us/images/shows/000/060/360/posters/medium/522aa0d783.jpg',
+      name: 'The Last Ship',
+      colspan: 1,
+      rowspan: 2
+   });
+   console.log(this.shows);*/
+
     /*$scope.shows = (function() {
       var trending = TrendingShows.query();
       var maxWatchers = 0;
