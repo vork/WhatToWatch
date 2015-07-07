197aef8389edd5424e1f8600ac208dad653ff9b3
Mark Boss <mark@zahlz.com>
2015-07-07 16:00:41 +0200

    Added some more trakt api calls

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -38,6 +38,67 @@ angular
       }
     );
   })
+  .factory('WatchedShows', function($resource) {
+    return $resource(
+      'https://api-v2launch.trakt.tv/users/:user/watched/shows',
+      {user:'@user'},
+      {
+        query: {
+          method:'GET',
+          isArray:true,
+          params: {
+            'extended':'full'
+          },
+          headers: {
+            'Content-type':'application/json',
+            'trakt-api-version':'2',
+            'trakt-api-key':'0c0a20790918c78c3a72c05da18de725509d9c4a428ad7dc0c4a523b7e33815c'
+          }
+        }
+      }
+    );
+  })
+  .factory('SeasonDetails', function($resource) {
+    return $resource(
+      'https://api-v2launch.trakt.tv/shows/:id/seasons',
+      {show:'@show'},
+      {
+        query: {
+          method:'GET',
+          isArray:true,
+          params: {
+            'extended':'full,episodes'
+          },
+          headers: {
+            'Content-type':'application/json',
+            'trakt-api-version':'2',
+            'trakt-api-key':'0c0a20790918c78c3a72c05da18de725509d9c4a428ad7dc0c4a523b7e33815c'
+          }
+        }
+      }
+    );
+  })
+  .factory('ShowDetails', function($resource) {
+    return $resource(
+      'https://api-v2launch.trakt.tv/shows/:id',
+      {show:'@show'},
+      {
+        query: {
+          method:'GET',
+          isArray:true,
+          params: {
+            'extended':'full,images'
+          },
+          headers: {
+            'Content-type':'application/json',
+            'trakt-api-version':'2',
+            'trakt-api-key':'0c0a20790918c78c3a72c05da18de725509d9c4a428ad7dc0c4a523b7e33815c'
+          }
+        }
+      }
+    );
+  })
+
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
